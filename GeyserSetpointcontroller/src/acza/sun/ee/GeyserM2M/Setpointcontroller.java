db98bb0445eb0d613b344004a3ba3cab4d1e54f8
Andrew Cloete <andrewhcloete@gmail.com>
2015-08-19 16:49:22 +0200

    Added config.properties.
    Fixed minor bug, involving an "application" notification from the NSCL.
    See point (1) in comments.

--- GeyserSetpointcontroller/src/acza/sun/ee/GeyserM2M/Setpointcontroller.java
+++ GeyserSetpointcontroller/src/acza/sun/ee/GeyserM2M/Setpointcontroller.java
@@ -17,10 +17,9 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -41,107 +40,116 @@ import org.json.simple.parser.ParseException;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 
-import acza.sun.ee.geyserM2M.GeyserApplication;
 import acza.sun.ee.geyserM2M.SCLapi;
 
 
 public class Setpointcontroller {
 
-	private static final String USAGE_STRING = "<NSCL base URL> <Auth> <aPoc IP> <aPoc PORT> <Deadband>";
-	
 	private static final Logger logger = LogManager.getLogger(Setpointcontroller.class);
 	private static SCLapi nscl;
 	private static Map<Long, Integer> geyser_setpoint_map = new ConcurrentHashMap<Long, Integer>();//<geyser_ID, setpiont> 
 	
+	private static String NSCL_BASE_URL;
+	private static String AUTH;
 	private static String APOC_URL;
 	private static int APOC_PORT;
 	private static String APOC;
 	
 	private static String app_ID = "Setpointcontroller";
-	private static String setpoint_app_URI = "setpointapp";
+	private static String setpoint_app_URI = "setpointapp"; //(1)
 	private static String setpoint_data_URI = "setpointdata";
 	private static String setpoint_settings_URI = "setpointsetting";
 	
-	private static int DEFAULT_SETPOINT = 45;
-	private static int DEADBAND = 1;
+	private static int DEFAULT_SETPOINT;
+	private static int DEADBAND;
 	
 	
 	public static void main(String[] args) {
-		// ---------------------- Sanity checking of command line arguments -------------------------------------------
-				if( args.length != 5)
-				{
-					System.out.println( "Usage: " +  USAGE_STRING) ;
-					return;
-				}
+		// ---------------------- Reading and sanity checking configuration parameters -------------------------------------------
 
-				final String NSCL_BASE_URL = args[0];//"52.10.236.177";//"localhost";//
+		Properties configFile = new Properties();
+		try {
+			configFile.load(Setpointcontroller.class.getClassLoader().getResourceAsStream("config.properties"));
 
-				final String AUTH = args[1];
+			NSCL_BASE_URL = configFile.getProperty("NSCL_BASE_URL");
+			AUTH = configFile.getProperty("AUTH");		
+			APOC_URL = configFile.getProperty("APOC_URL");	
 
-				APOC_URL = args[2];
-				try{
-					APOC_PORT = Integer.parseInt( args[3] ); // Convert the argument to ensure that is it valid
-				}catch ( Exception e ){
-					System.out.println( "aPoc port invalid." ) ;
-					return;
-				}
-				APOC = APOC_URL + ":" + APOC_PORT;
-				
-				try{
-					DEADBAND = Integer.parseInt( args[4] );
-				}catch ( Exception e ){
-					System.out.println( "Deadand invalid." ) ;
-					return;
-				}
-				//---------------------------------------------------------------------------------------------------------------
+			try{
+				APOC_PORT = Integer.parseInt(configFile.getProperty("APOC_PORT")); // Convert the argument to ensure that is it valid
+			}catch ( Exception e ){
+				System.out.println( "APOC_PORT parameter invalid." ) ;
+				return;
+			}
+			APOC = APOC_URL + ":" + APOC_PORT;
+			
+			try{
+				DEADBAND = Integer.parseInt( configFile.getProperty("DEADBAND") );
+			}catch ( Exception e ){
+				System.out.println( "DEADBAND parameter invalid." ) ;
+				return;
+			}
 
-				logger.info("GeyserSetpointcontroller usage: "+ USAGE_STRING);
-				logger.info("GeyserSetpointcontroller started with parameters: " + args[0] + " " + args[1] + " " + args[2] + " " + args[3] + " " + args[4]);
-				
-				/* ***************************** START APOC SERVER ************************************************/
-				Server server = new Server(APOC_PORT);
+			try{
+				DEFAULT_SETPOINT = Integer.parseInt( configFile.getProperty("DEFAULT_SETPOINT") );
+			}catch ( Exception e ){
+				System.out.println( "DEFAULT_SETPOINT parameter invalid." ) ;
+				return;
+			}
 
-				ServletHandler handler = new ServletHandler();
-				server.setHandler(handler);
+			logger.info("GeyserSetpointcontroller started with: " + configFile.toString());
 
-				// IMPORTANT:
-				// This is a raw Servlet, not a Servlet that has been configured
-				// through a web.xml @WebServlet annotation, or anything similar.
-				handler.addServletWithMapping(ApocServlet.class, "/*");
+		} catch (IOException e) {
+			logger.fatal("Error in configuration file \"config.properties\"", e);
+			return;
+		}
 
-				try {
-					server.start();
-					logger.info("Apoc server started.");
-				} catch (Exception e) {
-					logger.fatal("Apoc server failed to start.", e);
-					return;
-				}
-				/* ********************************************************************************************/
-				
-				
-				//nscl = new SCLapi("nscl", NSCL_IP_ADD, "8080", "admin:admin");
-				if(AUTH.equalsIgnoreCase("NONE"))
-					nscl = new SCLapi(NSCL_BASE_URL);	//OpenMTC
-				else
-					nscl = new SCLapi(NSCL_BASE_URL, AUTH); //OM2M
-				
-				nscl.registerApplication(app_ID);
-				
-				//Look for all existing GEYSER applications and subscribe to them.
-				List<String> appList = nscl.retrieveApplicationList();
-				for(String app : appList){
-					if(app.startsWith("geyser")){
-						long geyser_id = getGeyserIdFromString(app);
-						geyser_setpoint_map.put(geyser_id, DEFAULT_SETPOINT);
-						nscl.createContainer(app_ID, "SETPOINT_"+geyser_id);
-						nscl.createContentInstance(app_ID, "SETPOINT_"+geyser_id, String.valueOf(DEFAULT_SETPOINT));
-						nscl.subscribeToContent(app_ID, "SETPOINT_"+geyser_id, setpoint_settings_URI+"_"+geyser_id, APOC);
-						nscl.subscribeToContent(geyser_id, "DATA", setpoint_data_URI, APOC);
-					}
-					
-				}
-				
-				nscl.subscribeToApplications(setpoint_app_URI, APOC);
+		//---------------------------------------------------------------------------------------------------------------
+
+		/* ***************************** START APOC SERVER ************************************************/
+		Server server = new Server(APOC_PORT);
+
+		ServletHandler handler = new ServletHandler();
+		server.setHandler(handler);
+
+		// IMPORTANT:
+		// This is a raw Servlet, not a Servlet that has been configured
+		// through a web.xml @WebServlet annotation, or anything similar.
+		handler.addServletWithMapping(ApocServlet.class, "/*");
+
+		try {
+			server.start();
+			logger.info("Apoc server started.");
+		} catch (Exception e) {
+			logger.fatal("Apoc server failed to start.", e);
+			return;
+		}
+		/* ********************************************************************************************/
+
+
+		//nscl = new SCLapi("nscl", NSCL_IP_ADD, "8080", "admin:admin");
+		if(AUTH.equalsIgnoreCase("NONE"))
+			nscl = new SCLapi(NSCL_BASE_URL);	//OpenMTC
+		else
+			nscl = new SCLapi(NSCL_BASE_URL, AUTH); //OM2M
+
+		nscl.registerApplication(app_ID);
+
+		//Look for all existing GEYSER applications and subscribe to them.
+		List<String> appList = nscl.retrieveApplicationList();
+		for(String app : appList){
+			if(app.startsWith("geyser")){
+				long geyser_id = getGeyserIdFromString(app);
+				geyser_setpoint_map.put(geyser_id, DEFAULT_SETPOINT);
+				nscl.createContainer(app_ID, "SETPOINT_"+geyser_id);
+				nscl.createContentInstance(app_ID, "SETPOINT_"+geyser_id, String.valueOf(DEFAULT_SETPOINT));
+				nscl.subscribeToContent(app_ID, "SETPOINT_"+geyser_id, setpoint_settings_URI+"_"+geyser_id, APOC);
+				nscl.subscribeToContent(geyser_id, "DATA", setpoint_data_URI, APOC);
+			}
+
+		}
+
+		nscl.subscribeToApplications(setpoint_app_URI, APOC);
 
 	}
 	
@@ -169,7 +177,7 @@ public class Setpointcontroller {
 			System.out.println("Inbound notification: " + notify.getStatusCode() + " -- Request URI: " + requestURI);
 
 			String target_resource = requestURI.substring(requestURI.lastIndexOf("/")+1);
-			if(target_resource.equalsIgnoreCase(setpoint_app_URI)){
+			if(target_resource.equalsIgnoreCase("application")){ //(1)
 				Application app = (Application) xm.xmlToObject(new String(notify.getRepresentation().getValue(), StandardCharsets.ISO_8859_1));
 				if(app.getAppId().startsWith("geyser")){
 					if(notify.getStatusCode().equals(StatusCode.STATUS_CREATED)){
@@ -267,4 +275,14 @@ public class Setpointcontroller {
 		return jobj.get(key);
 	}
 }
+
+/*
+ * (1)
+ * Oops. I was inconsistent with my scheme for aPoC URI's. Compare "subscribeToApplications" with "subscribeToContent" in SCLapi.java
+ * to see the problem. Not such a big deal for now. But it might be confusing for future updates. 
+ * 
+ * 
+ * 
+ * 
+ */
 
